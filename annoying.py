04b7431d08255c2fc24606f927338ca1f2b93eea
fawzmehfil <fawzmehfil@gmail.com>
2023-11-17 22:47:49 -0500

    New File

---
+++ annoying.py
@@ -0,0 +1,11 @@
+L = [0,1,2,3,4,5]
+for i in range(3):
+    for j in L[i]:
+        print(j)
+
+#def annoying(L):
+#	total = 0
+#	for i in range(3):
+#	    for j in L[i]:
+#		total += L[L[i][j]][L[j][i]]
+#	return total
